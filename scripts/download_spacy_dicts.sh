e6a6cf261b5e16ead246c6eb09d5b747e129d95d
reinaldomaslim <e0403962@u.nus.edu>
2020-05-01 10:53:42 +0800

    extract data

--- scripts/download_spacy_dicts.sh
+++ scripts/download_spacy_dicts.sh
@@ -1,7 +1,7 @@
-# python -m spacy download en
-# python -m spacy download es
-# python -m spacy download fr
+python -m spacy download en
+python -m spacy download es
+python -m spacy download fr
 
-python -m spacy download en_core_web_md
-python -m spacy download es_core_web_md
-python -m spacy download fr_core_web_md
+# python -m spacy download en_core_web_md
+# python -m spacy download es_core_web_md
+# python -m spacy download fr_core_web_md
